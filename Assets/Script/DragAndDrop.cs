561abada693f4d14118a4f3210184b640b184370
ALIENWARENB\amirb <amir.basyouni@gmail.com>
2017-02-16 12:36:55 +0100

    iniziata griglia con posizionamento oggetti

---
+++ Assets/Script/DragAndDrop.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public delegate void TurretEvent(DragAndDrop t);
+    public static TurretEvent OnDropTurret;
+
+    Vector3 dist;
+    float posX;
+    float posY;
+
+
+    void OnMouseDown()
+    {
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 currentPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, dist.z);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(currentPos);
+        transform.position = worldPos;
+    }
+
+    private void OnMouseUp()
+    {
+        if (OnDropTurret != null)
+        {
+            OnDropTurret(this);
+        }
+    }
+}
--- Assets/Script/TurretBase.cs
+++ Assets/Script/TurretBase.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurretBase : MonoBehaviour {
+public class TurretBase : MonoBehaviour, IPositionable
+{
+    
+    private void OnEnable()
+    {
+        DragAndDrop.OnDropTurret += TurretDrop;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void TurretDrop(DragAndDrop t)
+    {
+        //transform.position = new Vector3(0, 0);
+        //prima di togliere il drag and drop dovrebbe controllare di essere stato posizionato giusto
+         Destroy(t);
+    }
+
+
+
+    /// <summary>
+    /// Cambia la posizione dell'oggetto
+    /// </summary>
+    /// <param name="newPosition"></param>
+    public void SetPosition(Transform newPosition)
+    {
+        transform.SetParent(newPosition);
+        transform.position = newPosition.position;
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.GetComponent<Ground>() != null)
+        {
+            SetPosition(other.transform);
+            
+
+
+        }
+       
+    }
+
 }
