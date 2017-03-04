f4d9fe147c3920d7f4459cad0e08f6dd01ab9e2a
Viviana Figus <viviana.figus@gmail.com>
2017-03-04 16:59:39 +0100

    aggiunto menu per passare da una scena test all'altra

---
+++ Assets/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour {
+    public Button GridButton, ResourcesButton;
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void GoToGridScene() {
+        SceneManager.LoadScene("TestGridScene");
+    }
+    public void GoToResourcesScene() {
+        SceneManager.LoadScene("TestPlayerScene");
+    }
+
+}
