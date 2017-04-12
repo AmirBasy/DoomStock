ede6ee005d20b5930cfe87cc05d7e93d03fad764
Luca Garbelli <lu.gbelli@hotmail.it>
2017-04-12 12:39:11 +0200

    - aggiunto xinput e player input (non collegato al gioco)

---
+++ Assets/Script/Player/PlayerInput.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+namespace Framework
+{
+    public class PlayerInput
+    {
+        // Variabili per il funzionamento dei controller
+        PlayerIndex playerIndex;
+        GamePadState state;
+        GamePadState prevState;
+
+        public PlayerInput(PlayerIndex _playerIndex)
+        {
+            playerIndex = _playerIndex;
+        }
+
+        #region API
+        /// <summary>
+        /// Ritorna l'input del player nella forma di struttura
+        /// </summary>
+        /// <returns></returns>
+        public InputStatus GetPlayerInputStatus()
+        {
+            InputStatus inputStatus = ControllerInput();
+            if (!inputStatus.IsConnected)
+                inputStatus = KeyboardInput();
+
+            return inputStatus;
+        }
+        #endregion
+
+        #region ControllerInput
+        /// <summary>
+        /// Controlla l'input da controller (usando il plugin XInputDotNetPure)
+        /// </summary>
+        InputStatus ControllerInput()
+        {
+            InputStatus inputStatus = new InputStatus();
+
+            prevState = state;
+            state = GamePad.GetState(playerIndex);
+
+            // controlla se il controller è connesso
+            if (!state.IsConnected)
+            {
+                inputStatus.IsConnected = state.IsConnected;
+                return inputStatus;
+            }
+            else
+                inputStatus.IsConnected = state.IsConnected;
+
+            // left stick axis => equivalenti ai DPad
+            inputStatus.LeftThumbSticksAxisX = state.ThumbSticks.Left.X;
+            inputStatus.LeftThumbSticksAxisY = state.ThumbSticks.Left.Y;
+
+            if (prevState.Buttons.A == XInputDotNetPure.ButtonState.Released && state.Buttons.A == XInputDotNetPure.ButtonState.Pressed)
+            {
+                // confirm
+                inputStatus.A = ButtonState.Pressed;
+            }
+
+            if (prevState.Buttons.B == XInputDotNetPure.ButtonState.Released && state.Buttons.B == XInputDotNetPure.ButtonState.Pressed)
+            {
+                // go back
+                inputStatus.B = ButtonState.Pressed;
+            }
+
+            if (prevState.Buttons.X == XInputDotNetPure.ButtonState.Released && state.Buttons.X == XInputDotNetPure.ButtonState.Pressed)
+            {
+                // population menu
+                inputStatus.X = ButtonState.Pressed;
+            }
+
+            if (prevState.DPad.Up == XInputDotNetPure.ButtonState.Released && state.DPad.Up == XInputDotNetPure.ButtonState.Pressed)
+            {
+                // up
+                inputStatus.DPadUp = ButtonState.Pressed;
+            }
+
+            if (prevState.DPad.Left == XInputDotNetPure.ButtonState.Released && state.DPad.Left == XInputDotNetPure.ButtonState.Pressed)
+            {
+                // left
+                inputStatus.DPadLeft = ButtonState.Pressed;
+            }
+
+            if (prevState.DPad.Down == XInputDotNetPure.ButtonState.Released && state.DPad.Down == XInputDotNetPure.ButtonState.Pressed)
+            {
+                // down
+                inputStatus.DPadDown = ButtonState.Pressed;
+            }
+
+            if (prevState.DPad.Right == XInputDotNetPure.ButtonState.Released && state.DPad.Right == XInputDotNetPure.ButtonState.Pressed)
+            {
+                // right
+                inputStatus.DPadRight = ButtonState.Pressed;
+            }
+
+            return inputStatus;
+        }
+        #endregion
+
+        #region KeyboardInput
+        /// <summary>
+        /// Controlla l'input da tastiera (la tastiera non funziona se è collegato il controller dello stesso player Index)
+        /// </summary>
+        InputStatus KeyboardInput()
+        {
+            InputStatus inputStatus = new InputStatus();
+
+            switch (playerIndex)
+            {
+                case PlayerIndex.One:
+                    #region Player One Input
+                    if (Input.GetKeyDown(KeyCode.Z))
+                    {
+                        // Confirm
+                        inputStatus.A = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        // Go Back
+                        inputStatus.B = ButtonState.Pressed;
+                    }
+                    if (Input.GetKeyDown(KeyCode.X))
+                    {
+                        // Population Menu
+                        inputStatus.X = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.W))
+                    {
+                        // Up
+                        inputStatus.DPadUp = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.A))
+                    {
+                        // left
+                        inputStatus.DPadLeft = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.S))
+                    {
+                        // down
+                        inputStatus.DPadDown = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.D))
+                    {
+                        // right
+                        inputStatus.DPadRight = ButtonState.Pressed;
+                    }
+                    #endregion
+                    break;
+                case PlayerIndex.Two:
+                    #region Player Two Input
+                    if (Input.GetKeyDown(KeyCode.N))
+                    {
+                        // Confirm
+                        inputStatus.A = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.O))
+                    {
+                        // Go Back
+                        inputStatus.B = ButtonState.Pressed;
+                    }
+                    if (Input.GetKeyDown(KeyCode.U))
+                    {
+                        // Population Menu
+                        inputStatus.X = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.I))
+                    {
+                        // Up
+                        inputStatus.DPadUp = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.J))
+                    {
+                        // left
+                        inputStatus.DPadLeft = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.K))
+                    {
+                        // down
+                        inputStatus.DPadDown = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.L))
+                    {
+                        // right
+                        inputStatus.DPadRight = ButtonState.Pressed;
+                    }
+                    #endregion
+                    break;
+                case PlayerIndex.Three:
+                    #region Player Three Input
+                    if (Input.GetKeyDown(KeyCode.Home))
+                    {
+                        // Confirm
+                        inputStatus.A = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.PageDown))
+                    {
+                        // Go Back
+                        inputStatus.B = ButtonState.Pressed;
+                    }
+                    if (Input.GetKeyDown(KeyCode.PageUp))
+                    {
+                        // Population Menu
+                        inputStatus.X = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
+                    {
+                        // Up
+                        inputStatus.DPadUp = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        // left
+                        inputStatus.DPadLeft = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        // down
+                        inputStatus.DPadDown = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+                    {
+                        // right
+                        inputStatus.DPadRight = ButtonState.Pressed;
+                    }
+                    #endregion
+                    break;
+                case PlayerIndex.Four:
+                    #region Player Four Input
+                    if (Input.GetKeyDown(KeyCode.KeypadMultiply))
+                    {
+                        // Confirm
+                        inputStatus.A = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.KeypadMinus))
+                    {
+                        // Go Back
+                        inputStatus.B = ButtonState.Pressed;
+                    }
+                    if (Input.GetKeyDown(KeyCode.KeypadPlus))
+                    {
+                        // Population Menu
+                        inputStatus.X = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad8))
+                    {
+                        // Up
+                        inputStatus.DPadUp = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad4))
+                    {
+                        // left
+                        inputStatus.DPadLeft = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad5))
+                    {
+                        // down
+                        inputStatus.DPadDown = ButtonState.Pressed;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad6))
+                    {
+                        // right
+                        inputStatus.DPadRight = ButtonState.Pressed;
+                    }
+                    #endregion
+                    break;
+            }
+
+            return inputStatus;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Stato del bottone
+    /// </summary>
+    public enum ButtonState
+    {
+        Released = 0,
+        Pressed = 1,
+        Held = 2
+    }
+
+    /// <summary>
+    /// Strutta che contine tutti i comandi del joystick
+    /// </summary>
+    public struct InputStatus
+    {
+        public bool IsConnected;
+
+        public float LeftTriggerAxis;
+        public float RightTriggerAxis;
+
+        public float LeftThumbSticksAxisX;
+        public float LeftThumbSticksAxisY;
+
+        public float RightThumbSticksAxisX;
+        public float RightThumbSticksAxisY;
+
+        public ButtonState A;
+        public ButtonState B;
+        public ButtonState X;
+        public ButtonState Y;
+
+        public ButtonState LeftShoulder;
+        public ButtonState RightShoulder;
+
+        public ButtonState LeftThumbSticks;
+        public ButtonState RightThumbSticks;
+
+        public ButtonState DPadUp;
+        public ButtonState DPadLeft;
+        public ButtonState DPadDown;
+        public ButtonState DPadRight;
+
+        public ButtonState Start;
+        public ButtonState Select;
+    }
+
+    public class Player
+    {
+        public PlayerIndex playerIndex;
+        PlayerInput playerInput;
+
+        public Player(PlayerIndex _playerIndex)
+        {
+            playerIndex = _playerIndex;
+            playerInput = new PlayerInput(playerIndex);
+        }
+
+        /// <summary>
+        /// Funzione sostitutiva all'update
+        /// </summary>
+        public void OnUpdate()
+        {
+            CheckMenuInputStatus(playerInput.GetPlayerInputStatus());
+        }
+
+        bool isReleased = true;
+
+        /// <summary>
+        /// Controlla l'inpunt da passare al menù corrente o alla griglia
+        /// </summary>
+        /// <param name="_inputStatus"></param>
+        void CheckMenuInputStatus(InputStatus _inputStatus)
+        {
+            if (_inputStatus.LeftThumbSticksAxisY == 0)
+                isReleased = true;
+
+            if ((_inputStatus.DPadUp == ButtonState.Pressed || _inputStatus.LeftThumbSticksAxisY == 1) && isReleased)
+            {
+                isReleased = false;
+                // GO UP
+            }
+
+            if ((_inputStatus.DPadDown == ButtonState.Pressed || _inputStatus.LeftThumbSticksAxisY == -1) && isReleased)
+            {
+                isReleased = false;
+                // GO DOWN
+            }
+
+            if ((_inputStatus.DPadRight == ButtonState.Pressed || _inputStatus.LeftThumbSticksAxisX == 1) && isReleased)
+            {
+                isReleased = false;
+                // GO RIGHT
+            }
+
+            if ((_inputStatus.DPadLeft == ButtonState.Pressed || _inputStatus.LeftThumbSticksAxisX == -1) && isReleased)
+            {
+                isReleased = false;
+                // GO LEFT
+            }
+
+            if (_inputStatus.A == ButtonState.Pressed)
+            {
+                //FORWARD
+            }
+
+
+            if (_inputStatus.B == ButtonState.Pressed)
+            {
+                //BACKWARD
+            }
+
+            if (_inputStatus.X == ButtonState.Pressed)
+            {
+                //POPULATION MENU
+            }
+        }
+    }
+}
