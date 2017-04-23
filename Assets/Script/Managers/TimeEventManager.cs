27880535fc238031a08da413a58d3d727a005b0c
ALIENWARE\Amir <amir.basyouni@gmail.com>
2017-04-23 12:09:29 +0200

    Scorrere del tempo la tacca dell-ultimo mese non si riempie perchè l'evento FineMese e FineAnno succedono nello stesso momento

--- Assets/Script/Managers/TimeEventManager.cs
+++ Assets/Script/Managers/TimeEventManager.cs
@@ -3,86 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class TimeEventManager : MonoBehaviour {
-
-    public Image[] month;
-    public int MounthCounter;
-    Text yearText;
-
-
-    /// <summary>
-    /// aumenta il contatore di uno ogni volta che passa un evento Mese
-    /// </summary>
-    public void IncreaseMounthCounter() {
-        foreach (TimedEventData item in EventsPrefab)
-        {
-            if (item.ID == "FineMese")
-            {
-                MounthCounter++;     
-            }
-        }
-        if (MounthCounter>=12)
-        {
-            MounthCounter = 0;
-        }
-    }
-
-    public void FillTheMounthImage() {
-        switch (MounthCounter)
-        {
-            case 1:
-                GetComponentInChildren<Image>();
-                month[0].color = Color.green;
-                break;
-            case 2:
-                GetComponentInChildren<Image>();
-                month[1].color = Color.green;
-                break;
-            case 3:
-                GetComponentInChildren<Image>();
-                month[2].color = Color.green;
-                break;
-            case 4:
-                GetComponentInChildren<Image>();
-                month[3].color = Color.green;
-                break;
-            case 5:
-                GetComponentInChildren<Image>();
-                month[4].color = Color.green;
-                break;
-            case 6:
-                GetComponentInChildren<Image>();
-                month[5].color = Color.green;
-                break;
-            case 7:
-                GetComponentInChildren<Image>();
-                month[6].color = Color.green;
-                break;
-            case 8:
-                GetComponentInChildren<Image>();
-                month[7].color = Color.green;
-                break;
-            case 9:
-                GetComponentInChildren<Image>();
-                month[8].color = Color.green;
-                break;
-            case 10:
-                GetComponentInChildren<Image>();
-                month[9].color = Color.green;
-                break;
-            case 11:
-                GetComponentInChildren<Image>();
-                month[10].color = Color.green;
-                break;
-            case 12:
-                GetComponentInChildren<Image>();
-                month[11].color = Color.green;
-                break;
-            default:
-                break;
-        }
-    }
+public class TimeEventManager : MonoBehaviour
+{
 
     #region Events
     public delegate void GameTimedEvent(TimedEventData _eventData);
@@ -96,7 +20,8 @@ public class TimeEventManager : MonoBehaviour {
 
     void Start()
     {
-        foreach (TimedEventData ev in EventsPrefab) {
+        foreach (TimedEventData ev in EventsPrefab)
+        {
             Events.Add(Instantiate(ev));
         }
     }
@@ -112,27 +37,29 @@ public class TimeEventManager : MonoBehaviour {
     int unitsEnded = 0;
 
     float currentTime = 0;
-    
-    private void Update() {
+
+    private void Update()
+    {
         if (Events == null)
             return;
         currentTime += Time.deltaTime;
-        if (currentTime > unitDuration) {
+        if (currentTime > unitDuration)
+        {
             currentTime = 0;
             unitsEnded++;
             UnitEnd();
         }
-
-        IncreaseMounthCounter();
-        FillTheMounthImage();
     }
 
     /// <summary>
     /// Chiamata quando scade una unità di tempo. Controllo se è necessario invocare un timed event.
     /// </summary>
-    void UnitEnd() {
-        foreach (var ev in Events) {
-            if (ev.TimeUnitEnded()) {
+    void UnitEnd()
+    {
+        foreach (var ev in Events)
+        {
+            if (ev.TimeUnitEnded())
+            {
                 // Scateno l'evento
                 if (OnEvent != null)
                     OnEvent(ev);
