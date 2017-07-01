31e3961fa3f44a716ab3da7ab1afeccf2ceffee7
ALIENWARENB\amirb <amir.basyouni@gmail.com>
2017-07-01 10:52:15 +0200

    Si puo uscire dai credits

---
+++ Assets/Script/Menu/CreditsScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XInputDotNetPure;
+public class CreditsScene : MonoBehaviour {
+
+    // Use this for initialization
+    public PlayerIndex InputPlayerIndex;
+    PlayerInput playerInput;
+
+    public PlayerIndex InputPlayerIndex1;
+    PlayerInput playerInput1;
+
+    public PlayerIndex InputPlayerIndex2;
+    PlayerInput playerInput2;
+
+
+    private void Start()
+    {
+        playerInput = new PlayerInput(InputPlayerIndex);
+        playerInput1 = new PlayerInput(InputPlayerIndex1);
+        playerInput2 = new PlayerInput(InputPlayerIndex2);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInputStatus(playerInput.GetPlayerInputStatus());
+        CheckInputStatus(playerInput1.GetPlayerInputStatus());
+        CheckInputStatus(playerInput2.GetPlayerInputStatus());
+    }
+
+    void CheckInputStatus(InputStatus _inputStatus)
+    {
+        if (_inputStatus.B == ButtonState.Pressed)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
